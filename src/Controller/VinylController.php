3383c3714acaf8032c4654a0ba9b35e5b9835a24
Akili Daniels <akili.daniels@att.net>
2023-10-22 17:50:44 -0700

    Services, Autowiring, Bind Arguments, Scoped HTTP Clients, Controllers, Env Vars

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -3,15 +3,20 @@
 namespace App\Controller;
 
 
+use App\Service\MixRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 use function Symfony\Component\String\u;
 // use Twig\Environment; Environment $twig
 
 class VinylController extends AbstractController
 {
+    public function __construct(
+        private MixRepository $mixRepository,
+        private bool $isDebug
+    ){}
+
     #[Route('/', name: 'app_homepage', methods: ['GET'])]
     public function homepage(): Response
     {
@@ -39,11 +44,12 @@ class VinylController extends AbstractController
     }
 
     #[Route('/browse/{slug}', name: 'app_browse', methods: ['GET'])]
-    public function browse(HttpClientInterface $httpClient, string $slug = null): Response
+    public function browse(string $slug = null): Response
     {
+        dump($this->isDebug);
+
         $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
-        $response = $httpClient->request("GET", "https://raw.githubusercontent.com/SymfonyCasts/vinyl-mixes/main/mixes.json");
-        $mixes = $response->toArray();
+        $mixes = $this->mixRepository->findAll();
 
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
