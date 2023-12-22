6a911ea2e64422acb1e7b48cbb9788751468a382
Akili Daniels <akili.daniels@att.net>
2023-12-21 21:35:24 -0800

    Created the show page

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\VinylMix;
+use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -29,5 +30,15 @@ class MixController extends AbstractController
             $mix->getId(),
             $mix->getTrackCount(),
         ));
+    }
+
+    #[Route('/new/{id}', name: 'app_mix_show')]
+    public function show(string $id, VinylMixRepository $mixRepository): Response
+    {
+        $mix = $mixRepository->find($id);
+        
+        return $this->render('mix/show.html.twig', [
+            'mix' => $mix,
+        ]);
     }
 }
